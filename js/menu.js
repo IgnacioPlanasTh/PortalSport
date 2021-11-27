783b3a27f31e782dee9db64fdffc16dce38e7fda
Ignacio Planas <80254262+IgnacioPlanasTh@users.noreply.github.com>
2021-11-27 21:20:21 +0100

    reestructuration noms fichiers

--- js/menu.js
+++ js/menu.js
@@ -8,31 +8,32 @@ function myFunction() {
     }
 }
 
-
 let path = window.location.pathname;
 let page = path.split("/").pop();
 
 
-let A_accueil=document.getElementById("acueil-link");
-let A_callisthenie=document.getElementById("calisthenics-link");
-let A_gym=document.getElementById("gym-link");
-let A_running=document.getElementById("running-link");
-let A_crossfit=document.getElementById("crossfit-link");
+let A_accueil=document.getElementById("Accueil-link");
+let A_callisthenie=document.getElementById("Callisthenie-link");
+let A_gym=document.getElementById("Fitness-link");
+let A_running=document.getElementById("Running-link");
+let A_crossfit=document.getElementById("Crossfit-link");
 let A_aPropos=document.getElementById("contact-link");
 let A_creerEntrainement=document.getElementById("creerEntrainement-link");
 
-if(page==="acueil.html"){
+if(page==="Accueil.html"){
     A_accueil.className+=" active";
-}else if(page==="calisthenics.html"){
+}else if(page==="Callisthenie.html"){
     A_callisthenie.className+=" active";
-}else if(page==="gym.html"){
+}else if(page==="Fitness.html"){
     A_gym.className+=" active";
-}else if(page==="crossfit.html"){
+}else if(page==="Crossfit.html"){
     A_crossfit.className+=" active";
-}else if(page==="running.html"){
+}else if(page==="Running.html"){
     A_running.className+=" active";
 }else if(page==="creerEntrainement.html"){
     A_creerEntrainement.className+=" active";
+}else if(page==="contact.html"){
+    A_aPropos.className+=" active";
 }
 
 // console.log(A_aPropos,A_accueil,A_callisthenie,A_creerEntrainement,A_gym,A_running,A_crossfit);
